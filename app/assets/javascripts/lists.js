641f95693c1212d30f929c462f167a23c68d20c1
Yuichi Nagayama <mail@a.yui7.tk>
2015-06-08 05:30:32 +0900

    remove old code

--- app/assets/javascripts/lists.js
+++ app/assets/javascripts/lists.js
@@ -18,30 +18,6 @@ $(function() {
     return alert(error);
   });
 
-  // $('#new_list').on('ajax:success', function(event, data, status, xhr) {
-  //   var text = $("#list_data").val();
-  //   var text2 = encodeURIComponent(text);
-  //   var newlistText = $("<td>").text(text);
-  //   var newDeleteButton = $("<td>").append($("<a>").attr("class", "btn btn-danger oide").attr("data-confirm", "削除しますか？").attr("data-method", "delete").attr("data-remote", "true").attr("data-lid", data.lid).attr("href", "/lists/" + data.key + "?lid=" + data.lid).html("×"));
-  //   $(".table").prepend($("<tr>").prepend(newDeleteButton).prepend(newlistText));
-  //   $("#list_data").val('');
-  //   return alert(data.lid);
-  // }).on('ajax:error', function(event, xhr, status, error) {
-  //   return alert(error);
-  // });
-
-  // $('.btn.btn-danger.oide').on('ajax:success', function(event, data, status, xhr) {
-  //   $("[data-lid=" + data.sub_key + "]").parents('tr').fadeOut('slow', function() {
-  //     $(this).remove();
-  //   });
-  //   return alert(data.sub_key);
-  // }).on('ajax:error', function(event, xhr, status, error) {
-  //   return alert(error);
-  // }).on('ajax:complete', function(event, xhr, status, error) {
-  //   return alert(error);
-  // });
-
-
   $(document).on('click', '.del-lid', function(){
     $(this).parent('div').fadeOut('slow', function() {
       $(this).remove();
